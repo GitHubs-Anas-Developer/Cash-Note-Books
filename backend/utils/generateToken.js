af8d4e801ea9d27d2bc506fe51eaaa93ac03433c
GitHubs-Anas-Developer <anascodingdeveloper@gmail.com>
2025-03-25 21:11:01 +0400

    updated

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -13,10 +13,8 @@ const generateToken = (userId, res) => {
   // Set the JWT as an HTTP-only cookie
   res.cookie("jwt", token, {
     httpOnly: true, // Prevents JavaScript access (XSS protection)
-    secure: process.env.NODE_ENV === "production", // Use secure cookies in production
     sameSite: "strict", // CSRF protection
     maxAge: 15 * 24 * 60 * 60 * 1000, // 15 days in milliseconds
-    path: "/", // Cookie accessible across the entire site
   });
 };
 
